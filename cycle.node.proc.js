2c1b68ba17591e796b87818c25baf7eaba0d2fcb
Julian <pgmjah@gmail.com>
2016-01-14 18:42:17 -0500

    Cleaned up code.

--- cycle.node.proc.js
+++ cycle.node.proc.js
@@ -8,27 +8,25 @@ function CycleNodeProcess()
 	EventEmitter.call(this);
 	this._args = require("./args.js");
 	this._fsw = fs.watch(this._args.process, this.startProcess.bind(this));
-	console.log(util.format("CycleNodeProcess will recycle '%s' on change to file.", this._args.process));
+	console.log(util.format("Cycle: Listening to '%s'", this._args.process));
 	process.title = util.format("CycleNodeProcess - Process '%s'", this._args.process);
 
-	this.startProcess();
+	this.startProcess(this._args.arArgs);
 }
 util.inherits(CycleNodeProcess, EventEmitter);
 var _p = CycleNodeProcess.prototype;
 
 _p._cp = null;
-_p.startProcess = function(event, filename)
+_p.startProcess = function(args, event, filename)
 {
-	
-	//var x = fs.statSync();
-	console.dir(arguments);
-
 	if(this._cp)
 		this._cp.kill();
+
+	console.log(util.format("Cycle: Starting '%s'", this._args.process));
 	this._cp = proc.fork(this._args.process, this._args.arArgs);
 	this._cp.on("exit", function(cp, code, signal)
 	{
-		//console.dir(cp);//console.log(util.format("Process '%s' Killed...restarting...", this._args.process));
+		console.log(util.format("Cycle: Killed '%s'", this._args.process));
 	}.bind(this, this._cp));
 	return this._cp
 }
