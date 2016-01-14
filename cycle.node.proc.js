92f286274dd0c395fc49eae8344b6f2fbe264355
Julian <pgmjah@gmail.com>
2016-01-14 14:26:03 -0500

    .

--- cycle.node.proc.js
+++ cycle.node.proc.js
@@ -10,6 +10,7 @@ function CycleNodeProcess()
 	this.startProcess();
 	this._fsw = fs.watch(this._args.process, this._onProcFileChange.bind(this));
 	console.log(util.format("CycleNodeProcess will recycle '%s' on change to file.", this._args.process));
+	process.title = util.format("CycleNodeProcess - Process '%s'", this._args.process);
 }
 util.inherits(CycleNodeProcess, EventEmitter);
 var _p = CycleNodeProcess.prototype;
@@ -23,12 +24,16 @@ _p._cp = null;
 _p.startProcess = function()
 {
 	if(this._cp)
+	{
 		this._cp.kill();
+	}
 
 	this._cp = proc.fork(this._args.process, this._args.arArgs);
 	this._cp.on("exit", (function(code, signal)
 	{
+		console.dir(arguments);
 		console.log(util.format("Process '%s' Killed...restarting...", this._args.process));
 	}).bind(this));
 }
 var cProcess = new CycleNodeProcess();
+
--- proxy.server.js
+++ proxy.server.js
@@ -1,16 +1,21 @@
-var http = require("http");
-var url = require("url");
-var util = require("util");
-var args = require("./args.js");
+const http = require("http");
+const url = require("url");
+const util = require("util");
+const args = require("./args.js");
 
 var server = http.createServer(function(request, response)
 {
+	if(this._cr)
+	{
+		this._cr.abort();
+		this._cr = null;
+	}
+
 	var query = url.parse(request.url, true).query;
 	if(query.uri)
 	{
-		console.log(util.format("Proxy To: %s", request.url));
 		var info = url.parse(query.uri);
-		var clientRequest = http.request(info, function(srvRequest, srvResponse, response)
+		var clientRequest = this._cr = http.request(info, function(srvRequest, srvResponse, response)
 		{
 			srvResponse.writeHead(200, "OK", {"content-type":"text/html", "access-control-allow-origin":"*"});
 			response.on("data", function(chunk)
@@ -23,14 +28,59 @@ var server = http.createServer(function(request, response)
 				console.log("Proxy Returned");
 			});
 		}.bind(clientRequest, request, response));
+		clientRequest.on("socket", function(response, socket, head)
+		{
+			console.log(util.format("Proxy To: %s", request.url));
+		});
+		clientRequest.on("error", function(error)
+		{
+			console.log(util.format("500 Internal Server Error: %s (%s)", error.code, error.errno));
+			response.writeHead(500, "Internal Server Error", {"content-type":"text/html", "access-control-allow-origin":"*"});
+			response.end(util.format("<!DOCTYPE html><html><body><h1>Status: 500</h1><h2>Internal Server Error: %s</h2></body</html>", error.toString()));
+		});
 		clientRequest.end();
 	}
 	else
 	{
 		console.log("404 Not found: " + request.url + "");
-		response.writeHead(404, "Not Found", {});
-		response.end("<!DOCTYPE html><html><body><h1>Status: 404</h1><h2>Resource not found: " + request.url + "</h2></body</html>");
+		response.writeHead(404, "Not Found", {"content-type":"text/html", "access-control-allow-origin":"*"});
+		response.end(util.format("<!DOCTYPE html><html><body><h1>Status: 404</h1><h2>Resource not found: %s</h2></body</html>", request.url));
 	}
-}).listen(args.port);
-console.log(util.format("PROXY server listening on http://172.1.0.1:%s/...(localhost/ServerName)", args.port));
+}).listen(args.port, function()
+{
+	process.title = util.format("Proxy server - listening on %s", args.port);
+	console.log(util.format("Proxy server listening on http://172.1.0.1:%s/...(localhost/ServerName)", args.port))
+});
+
+
+process.on("kill", function()
+{
+	console.log("kill");
+});
+process.on("beforeExit", function()
+{
+	console.log("beforeExit");
+});
+process.on("exit", function()
+{
+	console.log("exit");
+});
+process.on("message", function(message)
+{
+	console.log("message");
+});
+process.on("rejectHandler", function()
+{
+	console.log("rejectHandler");
+});
+process.on("uncaughtException", function()
+{
+	console.log("uncaughtException");
+});
+process.on("unhandledRejection", function()
+{
+	console.log("unhandledRejection");
+});
+
+
 
