bc9a490aa93ece60bb292329ba93a610aebb9849
Julian <pgmjah@gmail.com>
2016-01-22 14:40:43 -0500

    smarter cycling

--- cycle.node.proc.js
+++ cycle.node.proc.js
@@ -8,9 +8,12 @@ function CycleNodeProcess()
 	EventEmitter.call(this);
 	this._args = require("./args.js");
 	this._fsw = fs.watchFile(this._args.process, {interval: 250}, this.startProcess.bind(this));
-	console.log(util.format("Cycle: Listening to '%s'", this._args.process));
+	console.log(util.format("Cycler: Listening to '%s'", this._args.process));
 	process.title = util.format("CycleNodeProcess - Process '%s'", this._args);
+	
+	this._restarting = true;
 	this.startProcess(this._args.arArgs);
+	this._restarting = false;
 }
 util.inherits(CycleNodeProcess, EventEmitter);
 var _p = CycleNodeProcess.prototype;
@@ -27,11 +30,11 @@ _p.startProcess = function(curr, prev)
 		}
 		else
 		{
-			console.log(util.format("Cycle: Start '%s'", this._args.process));
+			console.log(util.format("Cycler: Start '%s'", this._args.process));
 			this._cp = proc.fork(this._args.process, process.argv.slice(2));
 			this._cp.on("close", function(code, signal)
 			{
-				console.log(util.format("Cycle: Stop '%s'", this._args.process));
+				console.log(util.format("Cycler: Stop '%s'", this._args.process));
 				this._restarting = true;
 				this.startProcess();
 				this._restarting = false;
