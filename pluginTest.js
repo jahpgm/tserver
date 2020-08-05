5b54e386e4019083250e8c3e91e1552c755c417d
pgmjah <pgmjah@gmail.com>
2020-08-05 16:34:54 -0400

    removed bad path call from pluginTest.js

--- pluginTest.js
+++ pluginTest.js
@@ -7,7 +7,6 @@ module.exports = {
 	},
 	preprocessData:(data, headers, filePath, srvPath)=>{
 		console.log(`Plugin - preprocessData filePath: ${filePath},  srvPath: ${srvPath}(${JSON.stringify(headers)})`);
-		const path = new paths.path(filePath);
 		return {data, headers};
 	}
 };
